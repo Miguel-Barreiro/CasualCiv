263d2e8f3c20c6afda40001fb47fcc30e977e54d
miguel barreiro <miguel.barreiro.mail@gmail.com>
2026-04-15 14:36:15 +0100

    added minor stats

--- Project/Assets/!/Content/Game/GameStatsContainer.cs
+++ Project/Assets/!/Content/Game/GameStatsContainer.cs
@@ -1,10 +1,14 @@
 using Core.Model.Data;
+using Core.Model.Stats;
+using UnityEngine;
 
 namespace Game
 {
 	public sealed class GameStatsContainer : DataTable
 	{
-		
+		[SerializeField] private StatConfig day_stat;
+		public StatConfig Day => day_stat;
+
 		
 		
 	}
